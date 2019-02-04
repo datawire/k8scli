c0f17d9a7ee77c51c83eb98e7a9e1b0736f41cb9
Luke Shumaker <lukeshu@datawire.io>
2019-02-04 09:44:45 -0500

    initial commit

---
+++ client.go
@@ -0,0 +1,71 @@
+package k8s_cli
+
+import (
+	"net/http"
+
+	"github.com/ericchiang/k8s"
+	"github.com/spf13/pflag"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/rest"
+
+	// Import to initialize client auth plugins.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
+
+// NewOutOfClusterClientFactory is a counterpart to
+// k8s.NewInClusterClient().  It adds the appropriate kubeconfig flags
+// to the FlagSet, and returns a function to call after
+// FlagSet.Parse() that will read those flags and the kubeconfig file
+// to construct a client.
+func NewOutOfClusterClientFactory(flags *pflag.FlagSet) func() (*k8s.Client, error) {
+	configFlags := genericclioptions.NewConfigFlags()
+
+	// We can disable or enable flags by setting them to
+	// nil/non-nil prior to calling .AddFlags().
+	//
+	// .Username and .Password are already disabled by default in
+	// genericclioptions.NewConfigFlags().
+	//
+	// Unlike client-go/discovery.CachedDiscoveryClient,
+	// ericchiang/k8s doesn't support caching to the filesystem,
+	// so disable the '--cache-dir' flag.
+	configFlags.CacheDir = nil
+
+	configFlags.AddFlags(flags)
+
+	return func() (*k8s.Client, error) {
+		configLoader := configFlags.ToRawKubeConfigLoader()
+
+		namespace, _, err := configLoader.Namespace()
+		if err != nil {
+			return nil, err
+		}
+
+		restconfig, err := configLoader.ClientConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		endpoint, _, err := rest.DefaultServerURL(restconfig.Host,
+			"", schema.GroupVersion{},
+			rest.IsConfigTransportTLS(*restconfig))
+		if err != nil {
+			return nil, err
+		}
+
+		transport, err := rest.TransportFor(restconfig)
+		if err != nil {
+			return nil, err
+		}
+
+		client := &k8s.Client{
+			Endpoint:  endpoint.String(),
+			Namespace: namespace,
+			Client: &http.Client{
+				Transport: transport,
+			},
+		}
+		return client, nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/datawire/k8s_cli
+
+require (
+	github.com/ericchiang/k8s v1.2.0
+	k8s.io/api kubernetes-1.13.3
+	k8s.io/apimachinery kubernetes-1.13.3
+	k8s.io/cli-runtime kubernetes-1.13.3
+	k8s.io/client-go v10.0.0
+)
